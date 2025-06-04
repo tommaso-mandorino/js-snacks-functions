cc94946d4d5673b04581f2aa5e20cb9d44acdafc
tommaso-mandorino <tommasomandorino@outlook.it>
2025-06-04 17:03:29 +0200

    feat(arrow function version): add arrow function version for snack 2

---
+++ snack-funzioni/02_iniziali/arrow.js
@@ -0,0 +1,19 @@
+/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+
+const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+
+
+// Dichiara la funzione qui.
+const firstLetterOf = array => {
+    const firstLetterArray = [];
+    for (let i = 0; i < array.length; i++) {
+        firstLetterArray.push(array[i][0]);
+    }
+    return firstLetterArray;
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+console.log(firstLetterOf(names));
+
+
+//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
