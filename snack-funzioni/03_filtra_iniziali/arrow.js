4ac5e9b2c7f6963834107a649ad588a32eaafe07
tommaso-mandorino <tommasomandorino@outlook.it>
2025-06-04 17:04:45 +0200

    feat(arrow function version): add arrow function version for snack 3

---
+++ snack-funzioni/03_filtra_iniziali/arrow.js
@@ -0,0 +1,21 @@
+/* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
+
+const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
+
+
+// Dichiara la funzione qui.
+const onlyStartsWith = (letter, array) => {
+    const filteredArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i][0] === letter) {
+            filteredArray.push(array[i]);
+        }
+    }
+    return filteredArray;
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+console.log(onlyStartsWith('A', names))
+
+
+//Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
