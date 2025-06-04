8fe454913a81737a29f26fc3dc190272224623de
tommaso-mandorino <tommasomandorino@outlook.it>
2025-06-04 16:17:42 +0200

    feat(standard version): finish snack 3

--- snack-funzioni/03_filtra_iniziali/script.js
+++ snack-funzioni/03_filtra_iniziali/script.js
@@ -4,10 +4,18 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function onlyStartsWith(letter, array) {
+    let filteredArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i][0] === letter) {
+            filteredArray.push(array[i]);
+        }
+    }
+    return filteredArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(onlyStartsWith('A', names))
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
