671228ce802af3ac7dfb5698990c6e67d6db8eb4
tommaso-mandorino <tommasomandorino@outlook.it>
2025-06-04 16:19:28 +0200

    feat(standard version): finish snack 4

--- snack-funzioni/04_conta_vocali/script.js
+++ snack-funzioni/04_conta_vocali/script.js
@@ -4,10 +4,22 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function countVowels(string) {
+    let vowelCount = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === 'a' ||
+            string[i] === 'e' ||
+            string[i] === 'i' ||
+            string[i] === 'o' ||
+            string[i] === 'u') {
+            vowelCount++;
+        }
+    }
+    return vowelCount;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(countVowels(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
