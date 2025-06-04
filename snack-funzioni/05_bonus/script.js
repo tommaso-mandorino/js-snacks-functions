e96bbc571078f6397206777b0305c00b8e19ae90
tommaso-mandorino <tommasomandorino@outlook.it>
2025-06-04 16:47:12 +0200

    feat(standard version): finish snack 5

--- snack-funzioni/05_bonus/script.js
+++ snack-funzioni/05_bonus/script.js
@@ -9,10 +9,26 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+function greet(name) {
 
+    const hour = new Date().getHours();
+
+    if (hour >= 0 && hour <= 13) {
+
+        return `Buongiorno ${name}`;
+
+    } else if (hour >=14 && hour <= 17) {
+
+        return `Buon pomeriggio ${name}`;
+
+    } else {
+
+        return `Buonasera ${name}`;
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(greet(name));
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
